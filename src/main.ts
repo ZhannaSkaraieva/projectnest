337e0ced071ddf92795b692aaecf73ddad17a84c
Zhanna Skaraieva <i.m.p.h.e.l.l@gmail.com>
2025-04-24 22:39:56 +0300

    Initial commit

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+//Файл входа приложения, который использует основную функцию NestFactoryдля создания экземпляра приложения Nest.
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { NestExpressApplication } from '@nestjs/platform-express';
+require('dotenv').config(); // загрузка переменных окружения из .env файла
+//npm install dotenv => ".env"
+
+
+//запускаем HTTP-прослушиватель, который позволяет приложению ожидать входящие HTTP-запросы.
+async function bootstrap() {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
